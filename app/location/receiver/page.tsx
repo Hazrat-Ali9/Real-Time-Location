1e1a7d0f36e1f9c819d279f8bf402d6a5d8cc541
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-12-15 18:03:29 +0600

    apps  :books:

---
+++ app/location/receiver/page.tsx
@@ -0,0 +1,12 @@
+import LocationReceiver from '@/components/location/LocationReceiver';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <LocationReceiver />
+        </div>
+    );
+};
+
+export default page;
---
+++ app/location/sender/page.tsx
@@ -0,0 +1,12 @@
+import LocationSender from '@/components/location/LocationSender';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <LocationSender />
+        </div>
+    );
+};
+
+export default page;
